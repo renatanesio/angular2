a1df01abc3f9ae2292a9019b595197bde15fab86
Renata Nésio <renata.nesio2@gmail.com>
2023-11-30 15:00:54 -0300

    Meu primeiro component

---
+++ src/app/meu-primeiro/meu-primeiro.componente.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-meu-primeiro',
+    template: `
+        <p>Este é meu primeiro component com Angular 2!</p>
+    
+    `
+})
+export class MeuPrimeiroComponent {
+
+}
