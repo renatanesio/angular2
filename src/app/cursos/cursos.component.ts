d22ceff2c1ae4e2898c5f53fe39ec39673e871c6
Renata Nésio <renata.nesio2@gmail.com>
2023-12-01 13:08:15 -0300

    Example of *ngFor directive

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
+
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string | undefined;
+  cursos: string[] = [
+    'CS50\'s Introduction to Game Development',
+    'CS50\'s Introduction to Artificial Intelligence with Python',
+    'CS50\'s Introduction to Programming with Scratch'
+  ]
+
+  constructor() {
+    this.nomePortal = 'https://pll.harvard.edu/catalog/free'
+  }
 
   ngOnInit(): void {
   }
-
 }
