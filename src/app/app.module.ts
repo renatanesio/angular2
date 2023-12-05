6fe1a3235c7d674896d390c15dfe2704d2b6b83e
Renata Nésio <renata.nesio2@gmail.com>
2023-12-05 07:43:20 -0300

    Add data-binding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.componente';
 import { CursosModule } from './cursos/cursos.module';
+import { DataBindingComponent } from './data-binding/data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeuPrimeiro2Component,
-    MeuPrimeiroComponent
+    MeuPrimeiroComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-binding/data-binding/data-binding.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+
+})
+
+
+export class DataBindingComponent implements OnInit {
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isMouseOver: boolean = false;
+
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor:string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
