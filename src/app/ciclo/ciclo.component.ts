15392cd5b238e2f11f5c5eb9557fdd18cdf02fc2
Renata Nésio <renata.nesio2@gmail.com>
2023-12-06 10:20:52 -0300

    Update ciclo component

--- src/app/ciclo/ciclo.component.ts
+++ src/app/ciclo/ciclo.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
   styleUrls: ['./ciclo.component.scss']
 })
-export class CicloComponent implements OnInit {
+export class CicloComponent implements
+OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  @Input() valorInicial: number = 10;
+
+
+  constructor() {
+    this.log('constructor');
+  }
+
+  ngOnChanges(): void {
+    this.log('ngOnChanges');
+  }
 
   ngOnInit(): void {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
   }
 
 }
