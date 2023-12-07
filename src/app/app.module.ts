11f5ea99c2af9c9bb1a464c365694139d24b0a0a
Renata Nésio <renata.nesio2@gmail.com>
2023-12-07 10:26:22 -0300

    Add ngif component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MeuFormModule } from "./meu-form/meu-form.module";
 import { InputPropertyComponent } from './input-property/input-property.component';
 import { OutputPropertyComponent } from './output-property/output-property.component';
 import { CicloComponent } from './ciclo/ciclo.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { CicloComponent } from './ciclo/ciclo.component';
     DataBindingComponent,
     InputPropertyComponent,
     OutputPropertyComponent,
-    CicloComponent
+    CicloComponent,
+    DiretivaNgifComponent
   ],
   providers: [],
   bootstrap: [AppComponent],
